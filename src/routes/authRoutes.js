7136e862fe1c2fb876781af4067da36085aef567
kaifdatascientist <gallekatukaif@gmail.com>
2026-02-12 17:03:55 +0530

    Revert "Add GET info responses for /register and /login to avoid 404 on browser GETs"
    This reverts commit 1bab3764a50c54edb377e68a2e312e2922d183b1.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,22 +3,6 @@ const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
-// Informational GET endpoints so visiting the routes in a browser
-// shows a helpful message instead of 404 (API uses POST for these).
-router.get("/register", (req, res) => {
-	res.status(200).json({
-		message:
-			"This endpoint accepts POST requests. Send POST /register with JSON {name, email, password} to create an account.",
-	});
-});
-
-router.get("/login", (req, res) => {
-	res.status(200).json({
-		message:
-			"This endpoint accepts POST requests. Send POST /login with JSON {email, password} to authenticate.",
-	});
-});
-
 router.post("/register", register);
 router.post("/login", login);
 
